62da1c08b42b104795a23eca44ca7f810cd45744
wildasari30 <panewildasari@gmail.com>
2023-06-02 21:42:51 +0700

    rental mobil

---
+++ resources/views/pages/merk/index.blade.php
@@ -0,0 +1,10 @@
+@extends('template.master')
+
+@section('content')
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Merk</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Merk</li>
+        </ol>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::get('/greeting', function () {
 Route::get('/mobil', [MobilController::class, 'index']);
 Route::get('/mobil/create', [MobilController::class, 'create']);
 Route::post('/mobil/simpan-data', [MobilController::class, 'store']);
+
+Route::get('/merk', [MerkController::class, 'index']);
